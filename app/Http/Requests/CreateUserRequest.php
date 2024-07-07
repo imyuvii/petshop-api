f4fe5da7c74a65b3d4c5b37b55e4b607ba7c5a82
Yuvrajsinh Jhala <png625@gmail.com>
2024-07-07 11:28:25 +0530

    changed email in user factory

--- app/Http/Requests/CreateUserRequest.php
+++ app/Http/Requests/CreateUserRequest.php
@@ -15,10 +15,10 @@ use Illuminate\Validation\Rule;
  *     @OA\Property(property="email", type="string", format="email", example="user@example.com"),
  *     @OA\Property(property="password", type="string", example="password"),
  *     @OA\Property(property="password_confirmation", type="string", example="password"),
- *     @OA\Property(property="avatar", type="string", nullable=true, example="http://example.com/avatar.jpg"),
+ *     @OA\Property(property="avatar", type="string", nullable=true, example="uuid"),
  *     @OA\Property(property="address", type="string", example="123 Main St"),
  *     @OA\Property(property="phone_number", type="string", example="+1234567890"),
- *     @OA\Property(property="is_marketing", type="boolean", example=false)
+ *     @OA\Property(property="is_marketing", type="int", example=0)
  * )
  */
 class CreateUserRequest extends FormRequest
